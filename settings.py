f341072d0fdb3398e5f44687d3fd9be05a252b10
Mutsumi <>
2025-06-09 23:30:09 -0400

    Add settings.py

---
+++ settings.py
@@ -0,0 +1,55 @@
+from collections.abc import Callable
+from pathlib import Path
+from sys import exit as sys_exit
+from typing import ClassVar, override
+
+from openai import BaseModel
+from pydantic import Field
+from pydantic_settings import BaseSettings, PydanticBaseSettingsSource, PyprojectTomlConfigSettingsSource
+
+
+class Settings(BaseSettings, pyproject_toml_table_header=("tool", "4tv-tumblrbot")):
+    class Env(BaseSettings, env_file=".env"):
+        tumblr_consumer_key: str
+        tumblr_consumer_secret: str
+        tumblr_oauth_token: str
+        tumblr_oauth_secret: str
+
+        openai_api_key: str
+        openai_model: str
+
+        blogname: str
+
+        def __init__(self) -> None:
+            super().__init__()
+
+    class Generation(BaseModel):
+        draft_count: int = Field(ge=0)
+        tags_chance: float = Field(ge=0)
+        max_num_tags: int = Field(ge=1)
+
+    class Training(BaseModel):
+        output_file: Path
+        expected_epochs: int = Field(gt=0)
+        token_price: float = Field(ge=0)
+
+    env: ClassVar = Env()
+    generation: Generation
+    training: Training
+
+    system_message: str
+    user_message: str
+    model_name: str
+
+    def __init__(self) -> None:
+        super().__init__()
+
+    @classmethod
+    @override
+    def settings_customise_sources(cls, settings_cls: type[BaseSettings], *args: object, **kwargs: object) -> tuple[PydanticBaseSettingsSource, ...]:
+        return (PyprojectTomlConfigSettingsSource(settings_cls),)
+
+
+def start(name: str, main: Callable[[Settings], None]) -> None:
+    if name == "__main__":
+        sys_exit(main(Settings()))

bc680432117315d68bfe358e484fed8faf77b2ee
MaidScientistIzutsumiMarin <>
2025-08-17 22:21:53 -0400

    Change the reblog message to account for the reblog list

--- src/tumblrbot/utils/models.py
+++ src/tumblrbot/utils/models.py
@@ -76,7 +76,7 @@ class Config(FileSyncSettings):
     tags_developer_message: str = Field("You will be provided with a block of text, and your task is to extract a very short list of the most important subjects from it.", description="The developer message used to generate tags.")
     reblog_blog_identifiers: list[str] = Field([], description="The identifiers of blogs that can be reblogged from when generating drafts.")
     reblog_chance: NonNegativeFloat = Field(0.05, description="The chance to generate a reblog of a random post. This will use more OpenAI tokens.")
-    reblog_user_message: str = Field("Please write a comical Tumblr post in response to the following post from your blog:", description="The prefix for the user message used to reblog posts.")
+    reblog_user_message: str = Field("Please write a comical Tumblr post in response to the following post:", description="The prefix for the user message used to reblog posts.")
 
     @classmethod
     @override

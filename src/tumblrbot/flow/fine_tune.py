a6ff4e12e2aead91834ee56d78116db18fae950a
MaidScientistIzutsumiMarin <>
2026-01-13 18:47:20 -0500

    Slight simplification

--- src/tumblrbot/flow/fine_tune.py
+++ src/tumblrbot/flow/fine_tune.py
@@ -136,6 +136,5 @@ class FineTuner(FlowClass):
                     yield 4 + len(encoding.encode(message.content))
 
     def get_cost_string(self, total_tokens: int) -> str:
-        c = CurrencyConverter()
-        cost = c.convert(self.config.token_price / 1000000 * total_tokens, "USD", localeconv()["int_curr_symbol"])
+        cost = CurrencyConverter().convert(self.config.token_price / 1000000 * total_tokens, "USD", localeconv()["int_curr_symbol"])
         return currency(cost, grouping=True)

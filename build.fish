dde12de0f88fb6c6077d48757252f2fb9651474d
MaidScientistIzutsumiMarin <>
2026-01-23 21:20:13 -0500

    Use uvx in build script

--- build.fish
+++ build.fish
@@ -1,4 +1,4 @@
 #!/usr/bin/env fish
 
-pyinstaller --noconfirm --onefile --name tumblrbot --hidden-import tiktoken_ext.openai_public --optimize 2 src/tumblrbot/__main__.py &&
+uvx pyinstaller --noconfirm --onefile --name tumblrbot --hidden-import tiktoken_ext.openai_public --optimize 2 src/tumblrbot/__main__.py &&
     rm tumblrbot.spec

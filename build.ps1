de53625442eadcce40943ccd9d1c2bf9a36388a6
MaidScientistIzutsumiMarin <>
2026-02-01 04:46:22 -0500

    Remove powershell build script

--- build.ps1
+++
@@ -1,8 +0,0 @@
-pyinstaller.exe `
-    --noconfirm `
-    --onefile `
-    --name tumblrbot `
-    --hidden-import tiktoken_ext.openai_public `
-    --optimize 2 `
-    src\tumblrbot\__main__.py &&
-Remove-Item tumblrbot.spec
